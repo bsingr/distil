07edcdd95ede6f53277689ebc79ddaa11c68bbf3
Jeff Watkins <jeff@metrocat.org>
2010-08-12 10:47:28 -0700

    Changed git fetching of remote projects to use clone instead of pull

--- lib/distil/project.rb
+++ lib/distil/project.rb
@@ -36,22 +36,11 @@ module Distil
       
       FileUtils.mkdir_p(path)
       Dir.chdir path do
-        init_cmd = "git init"
-        init_cmd+= " -q"
-        # init_cmd += " -q" if options[:quiet] and not $verbose
-        # puts init_cmd if $verbose
-        system(init_cmd)
-        # base_cmd = "git pull --depth 1 #{uri}"
-        base_cmd = "git pull #{uri}"
-        base_cmd+= " -q"
-        # base_cmd += " -q" if options[:quiet] and not $verbose
-        base_cmd += " #{options["version"]}" if options["version"]
-        # puts base_cmd if $verbose
-        if system(base_cmd)
-          # puts "removing: .git .gitignore" if $verbose
-          # FileUtils.rm_rf %w(.git .gitignore)
-        else
-          # rm_rf path
+        clone_cmd  = "git clone #{uri} ."
+        clone_cmd += " -q"
+        clone_cmd += " #{options["version"]}" if options["version"]
+        if !system(clone_cmd)
+          raise ValidationError.new("Failed to clone external project: #{uri}")
         end
       end
     end
